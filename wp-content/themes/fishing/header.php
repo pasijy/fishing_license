1ee843290ce84cfd7e66d14d93ad4bbb66ed2d54
Pasi Jylhä-Ollila <pasijy@ety-a2123b-20.edu.wks.metropolia.fi>
2017-02-17 09:16:10 +0200

    wpcontent to git

---
+++ wp-content/themes/fishing/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,400italic&subset=latin, latin-ext' rel='stylesheet' type='text/css'>
+    <link href="<?php echo get_template_directory_uri(); ?>/css/fonts/foundation-icons.css" rel="stylesheet">
+	<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet">
+    <title>Fishing test</title>
+    <?php wp_head; ?>
+</head>
+<body> <?php body_class(); ?>>
+    <header>
+        <?php get_search_form(); ?>
+    </header>
